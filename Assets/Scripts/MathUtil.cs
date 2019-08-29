ea5634afa8017063b436294f6fa293925e4ebc11
kram <marc.vinas@gmail.com>
2019-08-29 02:27:16 +0200

    dispensador de regals calculant objectiu versio 1

---
+++ Assets/Scripts/MathUtil.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System;
+
+public class MathUtil 
+{
+    public static Vector3 Parabola(Vector3 start, Vector3 end, float height, float t)
+    {
+        Func<float, float> f = x => -4 * height * x * x + 4 * height * x;
+
+        var mid = Vector3.Lerp(start, end, t);
+
+        return new Vector3(mid.x, f(t) + Mathf.Lerp(start.y, end.y, t), mid.z);
+    }
+
+
+    public static Vector3 CalculatedVelocity(Vector3 start, Vector3 end, float hight, float gravity)
+    {
+        float displacementY = end.y - start.y;
+        Vector3 displacemnentXZ = new Vector3(end.x - start.x, 0, end.z - start.z);
+        float time = Mathf.Sqrt(-2 * hight / gravity) + Mathf.Sqrt(2 * (displacementY - hight) / gravity);
+        Vector3 velocityY = Vector3.up * Mathf.Sqrt(-2 * gravity * hight);
+        Vector3 velocityXZ = displacemnentXZ /time;
+        return velocityXZ + velocityY * - Mathf.Sign(gravity);
+    }
+}
---
+++ Assets/Scripts/ThrowPresent.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class ThrowPresent : MonoBehaviour
+{
+
+    public Transform initialPosition;
+    public Transform destinationPosition;
+    public float hight = 25f;
+    public float gravity = -18f;
+
+    //public Transform initPosition;
+    public GameObject prefabPresent;
+  
+    private GameObject presentGO;
+    private Vector3 velocityCalculated;
+    private Rigidbody presentRigidBody;
+    //private PresentProjectileController presentPC;
+    private void Start()
+    {
+       
+      
+
+    }
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire2"))
+        {
+            presentGO = Instantiate(prefabPresent, transform.position, Quaternion.identity);
+
+           
+           // presentPC = presentGO.GetComponent<PresentProjectileController>();
+            //presentPC.InstantiateVariables(transform, destinationPosition, hight, gravity);
+            presentRigidBody = presentGO.GetComponent<Rigidbody>();
+            Physics.gravity = Vector3.up * gravity;
+            velocityCalculated = MathUtil.CalculatedVelocity(initialPosition.position, destinationPosition.position, hight, gravity);
+            presentRigidBody.useGravity = true;
+            presentRigidBody.velocity = velocityCalculated;
+        }
+    }
+}
